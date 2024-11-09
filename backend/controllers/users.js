e73d632550db3bf228368add228f34a7bf29b42b
Abdalelahj <abdalelahaljamal@gmail.com>
2024-11-09 06:01:26 +0300

    register

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const { pool } = require("../models/db");
-
+const bcrypt=require("bcrypt")
+const saltRounds=parseInt(process.env.SALT)
 const Register = async (req, res) => {
   const {
     userName,
@@ -12,7 +13,45 @@ const Register = async (req, res) => {
     profileImage,
     bio,
   } = req.body;
+  const role_id = "4"; //! create admin then switch to user
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const query = `INSERT INTO users (userName,
+      firstName,
+      lastName,
+      email,
+      password,
+      country,
+      dateOfBirth,
+      profileImage,
+      bio,
+       role_id) 
+       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`;
+    const data = [
+      userName,
+      firstName,
+      lastName,
+      email.toLowerCase(),
+      hashedPassword,
+      country,
+      dateOfBirth,
+      profileImage,
+      bio,
+      role_id,
+    ];
   
+  try {
+    const result= await pool.query(query,data)
+    res.status(200).json({
+      success: true,
+      message: "Account created successfully",
+    });
+  } catch (error) {
+    res.status(409).json({
+      success: false,
+      message: "The email already exists",
+      error:error.message,
+    });
+  }
 };
 
 const login = /*async*/ (req, res) => {};
