2f8cac8fc07bd52fab207fcf53eecc6a94aa13f6
ahmednazzall <nazzall.ahmed@gmail.com>
2024-11-09 19:36:12 +0300

    posts_Complete

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,6 +12,11 @@ const authentication = require('../middleware/authentication');
 const authorization = require('../middleware/authorization');
 
 postsRouter.post('/' , authentication , createPost)
+postsRouter.get('/' , authentication , getAllPosts)
+postsRouter.get('/:post_id/post' , authentication , getPostById)
+postsRouter.get('/:user_id/user' , authentication , getPostByUser)
+postsRouter.put('/:post_id' , authentication , updatePostById)
+
 module.exports = postsRouter;
 
 
