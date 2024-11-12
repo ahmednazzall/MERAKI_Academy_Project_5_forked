30fc304156c3c65260ad4253cd7e598f65de36c6
ahmednazzall <nazzall.ahmed@gmail.com>
2024-11-12 23:01:47 +0300

    Debuging

--- frontend/src/components/redux/reducers/sliceFollowers.jsx
+++ frontend/src/components/redux/reducers/sliceFollowers.jsx
@@ -1,4 +1,4 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import {createSlice} from "@reduxjs/toolkit"
 export const followerSlice = createSlice({
     name :"followers",
     initialState :{
