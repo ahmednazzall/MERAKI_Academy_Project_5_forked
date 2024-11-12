e72a27df2b97882b44cd0fdbbc651d94bbd5e60c
ahmednazzall <nazzall.ahmed@gmail.com>
2024-11-12 23:12:10 +0300

    Import Css Files

---
+++ frontend/src/components/comments/Comments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './comments.css'
+const Comments = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Comments
--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './search.css'
 const Search = () => {
   return (
     <div></div>
