97af4198288515a62df22407686445b20ec37bc3
SereenHanandeh <sereen.hanandeh11@gmail.com>
2024-11-14 02:12:33 +0300

    profile

--- frontend/src/components/redux/reducers/sliceUser.jsx
+++ frontend/src/components/redux/reducers/sliceUser.jsx
@@ -1,25 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 import { act } from "react";
-
-
 
 export const sliceUser = createSlice({
   name: "User",
   initialState: {
     users: [],
 
-
     token: localStorage.getItem("token") || null,
     isLoggedIn: localStorage.getItem("token") ? true : false,
-
   },
   reducers: {
     register: (state, action) => {
       state.users.push(action.payload);
-
     },
     login: (state, action) => {
       state.token = action.payload;
@@ -31,41 +24,38 @@ export const sliceUser = createSlice({
     },
     getUserById: (state, action) => {
       state.users = action.payload;
-    },
-
-    },
-    login: (state, action) => {
-      state.token = action.payload;
-      state.isLoggedIn = true;
-      localStorage.setItem("token", action.payload);
-    },
-    getAllUsers: (state, action) => {
-      state.users = action.payload;
-    },
-    getUserById: (state, action) => {
-      state.users = action.payload;
-    },
-
-    getUserByUserName: (state, action) => {
-      state.users = action.payload;
-    },
-    updateUserById: (state, action) => {
-      state.users = state.users.map((user) => {
-        if (user.user_id === action.payload.user_id) {
-          return action.payload;
-        }
-      });
-      return users;
-    },
-    SoftDeleteUserById: (state, action) => {
-      state.users = state.users.filter((user) => {
-        return user.user_id !== action.payload;
-      });
-      return users;
     },
   },
-});
+  login: (state, action) => {
+    state.token = action.payload;
+    state.isLoggedIn = true;
+    localStorage.setItem("token", action.payload);
+  },
+  getAllUsers: (state, action) => {
+    state.users = action.payload;
+  },
+  getUserById: (state, action) => {
+    state.users = action.payload;
+  },
 
+  getUserByUserName: (state, action) => {
+    state.users = action.payload;
+  },
+  updateUserById: (state, action) => {
+    state.users = state.users.map((user) => {
+      if (user.user_id === action.payload.user_id) {
+        return action.payload;
+      }
+    });
+    return users;
+  },
+  SoftDeleteUserById: (state, action) => {
+    state.users = state.users.filter((user) => {
+      return user.user_id !== action.payload;
+    });
+    return users;
+  },
+});
 
 export const {
   register,
@@ -76,7 +66,5 @@ export const {
   updateUserById,
   SoftDeleteUserById,
 } = sliceUser.actions;
-
-export const { register } = sliceUser.actions;
 
 export default sliceUser.reducer;
