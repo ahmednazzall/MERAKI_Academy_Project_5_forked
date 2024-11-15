02436acb8dbda7205235692302e3252d4c5dec09
SereenHanandeh <sereen.hanandeh11@gmail.com>
2024-11-15 20:25:16 +0300

    eventb

--- frontend/src/components/dashboard/Side.jsx
+++ frontend/src/components/dashboard/Side.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./sideStyle.css"
+import "./sideStyle.css";
 const Side = () => {
   return (
     <div>
-      <Link to={"/home/profile"} onClick={()=>{
-        if (localStorage.getItem('postId')) {
-          localStorage.removeItem('postId')
-        }
-      }}>profile</Link>
+      <Link
+        to={"/home/profile"}
+        onClick={() => {
+          if (localStorage.getItem("postId")) {
+            localStorage.removeItem("postId");
+          }
+        }}
+      >
+        profile
+      </Link>
       <br></br>
 
       <Link to={"/home/"}>Feeds</Link>
       <br></br>
       <Link to={"/home/explore"}>#Explore</Link>
+      <br></br>
 
-      <Link to={"/home/"} onClick={()=>{
-        if (localStorage.getItem('postId')) {
-          localStorage.removeItem('postId')
-        }
-      }}>Feeds</Link>
+      <Link
+        to={"/home/"}
+        onClick={() => {
+          if (localStorage.getItem("postId")) {
+            localStorage.removeItem("postId");
+          }
+        }}
+      >
+        Feeds
+      </Link>
+      <br></br>
 
-     
+      <Link
+        to={"/home/events"}
+        onClick={() => {
+          if (localStorage.getItem("eventId")) {
+            localStorage.removeItem("eventId");
+          }
+        }}
+      >
+        Events
+      </Link>
     </div>
   );
 };
