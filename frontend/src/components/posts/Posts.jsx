a672c98c88e0ca81153ededfa9503adb28f05e06
SereenHanandeh <sereen.hanandeh11@gmail.com>
2024-11-17 20:43:09 +0300

    up

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -2,41 +2,36 @@ import React, { useEffect, useState } from "react";
 import "./posts.css";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { setPosts } from "../redux/reducers/slicePosts";
 import { useNavigate } from "react-router-dom";
-import Search from "../search/Search";
+import { setPosts } from "../redux/reducers/slicePosts";
 
 const Posts = () => {
   const [addPost, setAddPost] = useState({});
   const postInfo = { image: addPost.image || null, body: addPost.body || null, video: addPost.video || null };
-  const userId = localStorage.getItem('user_id')
-  const [addPost , setaddPost] = useState({})
-  const postInfo = {image : addPost.image||null , body : addPost.body|| null ,video:addPost.video||null  }
   const navigate = useNavigate();
+
   const token = localStorage.getItem("token");
   const dispatch = useDispatch();
 
-  const posts = useSelector((state) => state.posts.posts);
+  const posts = useSelector((posts) => {
+    return posts.posts.posts;
+  });
 
-  // Fetch posts on component mount
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/followers/${userId}`, {
+      .get("http://localhost:5000/posts", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
       .then((res) => {
-     
-        
-        dispatch(setPosts(res.data.data));
+        dispatch(setPosts(res.data.Posts));
       })
       .catch((err) => {
-        console.error(err);
+        console.log(err);
       });
   }, [dispatch, token]);
 
-  // Handle save post
   const handleAddSave = (id) => {
     axios
       .post(
@@ -49,89 +44,96 @@ const Posts = () => {
         }
       )
       .then((res) => {
-        message.success("Post saved successfully!");
+        console.log(res);
       })
       .catch((err) => {
-        console.error(err);
-        message.error("Failed to save post.");
-      });
-  };
-
-  // Handle adding a new post
-  const handleAddPost = () => {
-    axios
-      .post("http://localhost:5000/posts", postInfo, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        message.success("Post created successfully!");
-        setAddPost({}); // Reset the input fields
-      })
-      .catch((err) => {
-        console.error(err);
-        message.error("Failed to create post.");
+        console.log(err);
       });
   };
 
   return (
     <div>
       {/* Search Section */}
-      <Search token={token} />
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search"
+          className="search-input"
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              navigate('./search');
+            }
+          }}
+        />
+        <button className="search-button" onClick={() => navigate('./search')}>
+          Search
+        </button>
+      </div>
 
       {/* Create Post Section */}
       <div className="createPost">
-        <Input.TextArea
+        <input
           placeholder="What's on your mind?"
-          value={addPost.body || ""}
           onChange={(e) => setAddPost({ ...addPost, body: e.target.value })}
-          rows={3}
-          className="create-post-input"
+          className="post-input"
         />
-        <Button type="primary" onClick={handleAddPost}>
+        <button
+          type="submit"
+          onClick={() => {
+            axios
+              .post("http://localhost:5000/posts", postInfo, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              })
+              .then((res) => {
+                console.log("Post Created Successfully");
+              })
+              .catch((err) => {
+                console.log("Error creating post:", err);
+              });
+          }}
+          className="post-button"
+        >
           Post
-        </Button>
+        </button>
       </div>
 
-      {/* Display Posts */}
-      <div className="posts-section">
-        {posts?.map((post, index) => (
-          <div key={index} className="post">
-            {post.profile_image ? (
-              <Avatar src={post.profile_image} className="post-avatar" />
-            ) : (
-              <Avatar icon={<UserOutlined />} className="post-avatar" />
-            )}
-            <div className="post-content">
-              <h3>{post.user_name}</h3>
-              <p>{post.body}</p>
-              <div className="post-actions">
-                <Button type="link" onClick={() => handleAddSave(post.post_id)}>
+      {/* Posts Display Section */}
+      {posts?.map((elem, ind) => {
+        return (
+          <div key={ind} className="post">
+            {/* Just for test */}
+            {elem.profile_image ? (
+              <img src={elem.profile_image} alt="Profile" className="profPic" />
+            ) : null}
+            <div className="innerPost">
+              <h3>{elem.user_name}</h3>
+              <p>{elem.body}</p>
+              <div className="btn-container">
+                <button className="btn" onClick={() => { handleAddSave(elem.post_id); }}>
                   Save Post
-                </Button>
-                <Button
-                  type="link"
+                </button>
+                <button
+                  className="btn"
                   onClick={() => {
-                    localStorage.setItem("postId", post.post_id);
+                    localStorage.setItem("postId", elem.post_id);
                     navigate("./comments");
                   }}
                 >
                   Comments
-                </Button>
-                <Button type="link">Like</Button>
+                </button>
+                <button className="btn">Like</button>
               </div>
             </div>
           </div>
-        ))}
-      </div>
+        );
+      })}
 
-      {/* Floating Help Button */}
-      <FloatButton
-        className="float-button"
-        icon={<QuestionCircleOutlined />}
-        type="primary"
-      />
+      {/* Floating Help Button (Custom Implementation) */}
+      <div className="float-button" onClick={() => alert('Help Clicked!')}>
+        <span className="float-button-icon">?</span>
+      </div>
     </div>
   );
 };
