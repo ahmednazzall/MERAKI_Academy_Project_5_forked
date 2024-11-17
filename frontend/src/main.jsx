76e55bae109c2ce7a8503edb0d5e780b9a109cb7
SereenHanandeh <sereen.hanandeh11@gmail.com>
2024-11-17 19:29:44 +0300

    search

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./components/redux/store.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
